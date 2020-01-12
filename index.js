fa360f4a55ce5dbfc58541220fdcd309480c5570
daramirezv <30914399+daramirezv@users.noreply.github.com>
2020-01-12 18:28:45 -0500

    prueba final

--- index.js
+++ index.js
@@ -38,26 +38,30 @@ document.getElementById("othernavbar").onclick = function() {
     document.getElementById("other").style.display = "block"; 
 }
 
-$("#ModalCard1").on('hidden.bs.modal', function (e) {
-    $("video").each(function () { this.pause() });
+$('#ModalCard1').on('hidden.bs.modal', function (e) {
+    $('#ModalCard1 iframe').attr("src", "https://www.youtube.com/embed/tIGn5Hsy98M");
 });
 
-$("#ModalCard2").on('hidden.bs.modal', function (e) {
-    $("video").each(function () { this.pause() });
+$('#ModalCard2').on('hidden.bs.modal', function (e) {
+    $('#ModalCard2 iframe').attr("src", "https://www.youtube.com/embed/YIsR0va2TeY");
 });
 
-$("#ModalCard3").on('hidden.bs.modal', function (e) {
-    $("video").each(function () { this.pause() });
+$('#ModalCard3').on('hidden.bs.modal', function (e) {
+    $('#ModalCard3 iframe').attr("src", "https://www.youtube.com/embed/VzX3vpX_hpU");
 });
 
-$("#ModalCard4").on('hidden.bs.modal', function (e) {
-    $("video").each(function () { this.pause() });
+$('#ModalCard4').on('hidden.bs.modal', function (e) {
+    $('#ModalCard4 iframe').attr("src", "https://www.youtube.com/embed/OCgvRkFoWPE");
 });
 
-$("#ModalCard5").on('hidden.bs.modal', function (e) {
-    $("video").each(function () { this.pause() });
+$('#ModalCard5').on('hidden.bs.modal', function (e) {
+    $('#ModalCard5 iframe').attr("src", "https://www.youtube.com/embed/rnmHWYhyu_g");
 });
 
-$("#ModalCard6").on('hidden.bs.modal', function (e) {
-    $("video").each(function () { this.pause() });
+$('.modalBug').on('show.bs.modal', function (e) {
+    $('nav.navbar').attr("style", "padding-right:0px;");
+});
+
+$('.modalBug').on('hidden.bs.modal', function (e) {
+    $('nav.navbar').attr("style", "padding-right:16px;");
 });
